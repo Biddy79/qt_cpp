8523b59f613418f7956116e4811b749e5205e024
Adam <a.beardsley79@gmail.com>
2020-02-01 13:50:00 +0000

    more added to Flow_Control project

--- Flow_Control/main.cpp
+++ Flow_Control/main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+
+
     //if statment
 
     int age = 0;
@@ -16,23 +18,50 @@ int main(int argc, char *argv[])
 
     qInfo() << "Your age is " << age;
 
-    //not we dont nee brakets if statment is all on one line
-    if(age < 17) qInfo() << "You are Not old enough to drink in the U.K ";
+    //not we dont need brakets if statment is all on one line
+    if(age < 17) qInfo() << "You are Not old enough to drink in the U.K. drinks will not be availabel ";
     else if (age == 18){
-        qInfo() << "You are just old enough to dring in the U.K ";
+        qInfo() << "You are just old enough to dring in the U.K. drinks will be availabel ";
     }
     else{
-        qInfo() << "You are old enough to drink in the U.K ";
+        qInfo() << "You are old enough to drink in the U.K. drinks will be availabel ";
     }
 
+
+
+    std::string passWord;
+    qInfo() << "Enter password: ";
+    std::cin >> passWord;
 
     //terrnary operator
 
-    int value;
-    qInfo() << "Enter the price of the item you are selling: ";
-    std::cin >> value;
-    value > 0 && value < 10 ? qInfo("You enter an accetabel ammount ") : qFatal("The ammount you entered was not an accetabel ammont ");
+    //if true            do this                      else do this
+    passWord == "moto" ? qInfo("Password correct ") : qFatal("Password Incorect !!!!!!! program stopped");
 
+
+
+
+
+    int seatRow;
+    qInfo() << "Enter number of row you require. Note only seat row 1, 2 and 3 are availabel !!";
+    std::cin >> seatRow;
+
+    //switch
+
+    switch (seatRow) {
+    case 1:
+        qInfo() << "You have sellected row one";
+    break;
+    case 2:
+        qInfo() << "You have sellected row Two ";
+    break;
+    case 3:
+        qInfo() << "You have sellected row three ";
+    break;
+    default:
+        qFatal("You have entered an incorrect value");
+
+    }
 
 
     return a.exec();
